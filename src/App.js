e0c802db8d02baf06da369e54602dbd875377c69
kimjihye058 <s2321@e-mirim.hs.kr>
2025-02-11 07:48:54 +0900

    [feat] : 이미지, 폰트 설정 및 start화면 디자인

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import './App.css';
+import Start from "./start/start";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        {/* <Route path="/" element={<Home />} /> */}
+        <Route path="/" element={<Start />} />
+        <Route path="*" element={<Navigate to="/" replace />} />  {/* 잘못된 경로일 경우 Main으로 리다이렉트 */}
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/start/start.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import "./start.css";
+
+const start = () => {
+  return (
+    <div className='start-background'>
+      <div className='start-header'>
+        {`사랑유스콰이어\n출석체크`}
+      </div>
+      <div className='start-notice'>
+        클릭하여 시작하기
+      </div>
+    </div>
+  )
+}
+
+export default start;

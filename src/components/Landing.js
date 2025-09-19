7d6fe6bca2aa088202829bbe43bedc8abb955d65
kimjihye058 <s2321@e-mirim.hs.kr>
2025-09-20 06:08:53 +0900

    [feat] : Landing 화면에 Notice 부분 깜빡이는 효과 넣기

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,9 +1,20 @@
-import { useEffect } from "react";
+import { useMemo, useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 
 export default function Landing() {
   const navigate = useNavigate();
+
+  const colors = useMemo(() => ["#FFFFFF", "#9B9B9B"], []);
+  const [colorIndex, setColorIndex] = useState(0);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setColorIndex((prevIndex) => (prevIndex + 1) % colors.length);
+    }, 1000);
+
+    return () => clearInterval(intervalId);
+  }, [colors.length]);
 
   useEffect(() => {
     const handleClick = () => {
@@ -21,13 +32,15 @@ export default function Landing() {
     <LandingBackgroundDiv>
       <LandingHeader>{`사랑유스콰이어\n출석체크`}</LandingHeader>
       <LandingImg src="./images/LOGO.png" alt="Logo" />
-      <LandingNotice>클릭하여 시작하기</LandingNotice>
+      <LandingNotice style={{ color: colors[colorIndex] }}>
+        클릭하여 시작하기
+      </LandingNotice>
     </LandingBackgroundDiv>
   );
 }
 
 export const LandingBackgroundDiv = styled.div`
-  background-color: #2A303F;
+  background-color: #2a303f;
 `;
 
 export const LandingHeader = styled.div`
@@ -49,7 +62,7 @@ export const LandingImg = styled.img`
 
 export const LandingNotice = styled.div`
   font-family: "PRETENDARD-REGULAR";
-  font-size: 50px;
+  font-size: 40px;
   color: #fff;
   text-align: center;
   padding-top: 50px;
